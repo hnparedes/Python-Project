db022ac0932eca489e2512f26d78d1fad19d38f5
Hugo <62671886+hnparedes@users.noreply.github.com>
2024-11-20 11:42:53 -0500

    Add model outline code comments

---
+++ src/spidam/model.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import librosa
+import soundfile as sf
+from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
+
+# class AudioModel:
+
+#   def __init__(self):
+#   self audio_data, sample_rate, and filename here
+
+#   def load_audio(self, file_path):
+#   Audio file proccessing code here
+
+#   def convert_to_wav(self, file_path):
+#   Conversion code here
+
+#   def clean_audio_data(self):
+#   Check and handle meta and multi-channel data
+
+#   def calculate_rt60(self):
+#   Apply calculations to determine RT60
+
+#   def highest_resonance(self):
+#   Find the peak frequency
+#   Returns highest peak or frequency details here
+
+#   def visualize_waveform(self):
+#   Plot the waveform
