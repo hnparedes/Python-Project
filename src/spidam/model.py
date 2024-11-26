247d1ceff6ff14c4e151aa9ab56d59b927e81128
Samuel Jones <sjones.nova@gmail.com>
2024-11-26 13:49:07 -0500

    RT60 + Caching Changes

--- src/spidam/model.py
+++ src/spidam/model.py
@@ -18,7 +18,7 @@ class Model:
         self._pxx: NDArray = None
         self._duration: int = None
  
-    def load_audio(self, filepath: str, output_directory: Optional[str] = None) -> None: 
+    def load_audio(self, filepath: str, output_directory: Optional[str] = None) -> None:
         # Ensure the file exists
         if not os.path.isfile(filepath):
             logging.critical(f"File {filepath} does not exist")
@@ -38,6 +38,12 @@ class Model:
 
         # Calculate the duration of the wav file
         self._duration = len(self._audio) / self._sample_rate
+
+        # Clear the function cache
+        self.get_frequencies.cache_clear()
+        self.calculate_rt60.cache_clear()
+        self.highest_resonance.cac
+        logging.info("Function cache cleared")
 
     def _convert_to_wav(self, filepath: str, output_directory: Optional[str]) -> None:    
         # If the output directory is not set, then use the current working directory
@@ -67,13 +73,43 @@ class Model:
 
         return (self._frequencies[low_mask], self._frequencies[mid_mask], self._frequencies[high_mask])
             
-    def calculate_rt60(self) -> Tuple[int, int, int, int]:
+    @functools.cache
+    def calculate_rt60(self, low_cutoff: int = 60, low_max: int = 250, mid_max: int = 5000, high_cutoff: int = 10000, decay_db: int = 60) -> Tuple[int, int, int, int]:
         """ Generate a tuple containing the low, mid, high, and average rt60 values
         """
-        pass
+        # Get the filtered frequencies and convert into a numpy array
+        filtered_frequencies: NDArray = np.vstack(self.get_frequencies(low_cutoff, low_max, mid_max, high_cutoff))
+
+        # Calculate energy arrays - I have no idea if this works lmao. Yall should really check this <3
+        power: NDArray = filtered_frequencies ** 2
+        power_rev: NDArray = np.flip(power)
+        energy: NDArray = np.flip(np.cumsum(power_rev, axis=1))
+        energy_db: NDArray = 10 * np.log10(energy / np.max(energy, axis=1))
+        l_energy, m_energy, h_energy = np.split(energy_db, 3)
+        
+        # There's almost certainly a better way to do this but I don't wanna figure this out rn <3
+        # Calculate the low RT60
+        li_decay = np.where(l_energy <= -decay_db)[0][0]
+        lt_decay = li_decay / self._sample_rate
+        l_rt60 = (60 / decay_db) * lt_decay
+
+        # Calculate the mid RT60
+        mi_decay = np.where(m_energy <= -decay_db)[0][0]
+        mt_decay = mi_decay / self._sample_rate
+        m_rt60 = (60 / decay_db) * mt_decay
+
+        # Calculate the high RT60
+        hi_decay = np.where(h_energy <= -decay_db)[0][0]
+        ht_decay = hi_decay / self._sample_rate
+        h_rt60 = (60 / decay_db) * ht_decay
+
+        # Calculate the average RT60
+        a_rt60 = (l_rt60 + m_rt60 + h_rt60) / 3
+
+        return (l_rt60, m_rt60, h_rt60, a_rt60)
+
 
     @property
-    @functools.cached_property
     def highest_resonance(self) -> int:
         return self._frequencies[np.argmax(self._pxx)]
         
@@ -102,3 +138,6 @@ class Model:
         """
         return self._duration
     
+    @property
+    def unfiltered_frequency(self) -> NDArray:
+        return self._frequencies
