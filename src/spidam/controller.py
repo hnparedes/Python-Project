9173e357567236be5b506a2b1b3ffc339413021f
j lopez <jaydenrlopez@gmail.com>
2024-11-26 18:19:42 -0500

    Created basic controller.py module and class definition for future code migration.

---
+++ src/spidam/controller.py
@@ -0,0 +1,5 @@
+#This module is completely unfinished and is a temporary definition to be updated and replaced later
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
