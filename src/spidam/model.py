6e85c2b7713250c475263b6f74f9d0faf466baa6
Hugo <62671886+hnparedes@users.noreply.github.com>
2024-11-27 15:32:23 -0500

    Fixed code spacing

--- src/spidam/model.py
+++ src/spidam/model.py
@@ -129,6 +129,7 @@ class Model:
     @property
     def unfiltered_frequency(self) -> NDArray:
         return self._frequencies
+        
     @property
     def unfiltered_frequency(self) -> NDArray:
         return self._frequencies
