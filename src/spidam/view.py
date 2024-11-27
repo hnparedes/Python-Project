bbd5b7107180be16087b07d7094d3d1e45a24518
Alexander Chheng <152345333+APChheng@users.noreply.github.com>
2024-11-26 19:39:37 -0500

    Update view.py
    Changed columns on several of the buttons along with alignment. Also changed the resolution of the window so it looks better overall.

--- src/spidam/view.py
+++ src/spidam/view.py
@@ -11,7 +11,7 @@ from matplotlib.figure import Figure
 import numpy as np
 from numpy.typing import NDArray
 
-import os
+
 
 
 #from pandas import options
@@ -30,7 +30,7 @@ class View:
         self.root = tk.Tk()
         self.root.title('Group 25 SPIDAM')
         self.root.resizable(False, False)
-        self.root.geometry('870x800')
+        self.root.geometry('850x750')
 
         self.model = model
 
@@ -48,10 +48,13 @@ class View:
         #
         ##This currently has an issue where things are not aligned correctly, will be fixed
         #
+
         self.mainframe= tkinter.ttk.Frame(self.root, padding='5 5 5 5')
-        self.mainframe.grid(row = 0, column = 0, sticky=('E' 'W' 'N' 'S'))
+        self.mainframe.grid(row = 0, column = 0, sticky=(''))
         #three columns to handle buttons
         self.mainframe.grid_columnconfigure((0,1,2),weight=1)
+
+
 
         ##Buttons (this will be moved to self.create_widgets()
         #
@@ -59,18 +62,17 @@ class View:
         #
         #Precursor data for label
         self.gfile = ''
-        self.filename =''
         wavfile = StringVar()
         wavfile.set('File Name: ')
         #Function definition
         def open_file():
             filetypes = (('wav files', '*.wav'), ('mpeg-4 files', '*.m4a'), ('All files', '*.*'))
-            self.filename = fd.askopenfilename(title='Open a file', initialdir='/', filetypes=filetypes)
-            self.gfile = os.path.basename(self.filename)
+            filename = fd.askopenfilename(title='Open a file', initialdir='/', filetypes=filetypes)
+            self.gfile = filename
             wavfile.set('File Name: ' + self.gfile)
         #Button definition and function call on button press
         _openfile_btn = tkinter.ttk.Button(self.mainframe, text='Open a File', command=open_file)
-        _openfile_btn.grid(row = 0, column = 0, sticky='W')
+        _openfile_btn.grid(row = 0, column = 0, sticky = 'W')
         # Filename label, will start empty and populate after Open file command
         self.filelabel = tkinter.ttk.Frame(self.mainframe, padding='5 5 5 5')
         self.filelabel.grid(row=1, column=1, sticky=('E' 'W' 'N' 'S'))
@@ -86,9 +88,9 @@ class View:
         resonance.set('Resonant Frequency: ___ Hz')
         rt60 = StringVar()
         rt60.set('Difference: _._s')
-        #Function definition
+
         def analyze_file():
-            self.model.load_audio(self.filename)
+            self.model.load_audio(self.gfile)
             #graph call
 
             timerec.set('File Length: ' + self.model.duration + 's')
@@ -99,7 +101,7 @@ class View:
 
         #Analyze file button
         _analyzefile_btn = tkinter.ttk.Button(self.mainframe, text='Analyze File', command=analyze_file)
-        _analyzefile_btn.grid(row = 0, column = 2, sticky='E')
+        _analyzefile_btn.grid(row = 1, column = 0, sticky = 'W')
 
 
 
@@ -116,7 +118,7 @@ class View:
         canvas = FigureCanvasTkAgg(fig, master=self.mainframe)
         canvas.draw()
 
-        canvas.get_tk_widget().grid(row = 2, column = 1, sticky = 'EW')
+        canvas.get_tk_widget().grid(row = 2, column = 1)
 
         #this function and process prevents pyplot.subplots() from preventing the function from exiting
         def _closer():
@@ -145,16 +147,16 @@ class View:
 
         #Waveform graph button, temporary command that will change graph window to a waveform plot
         _waveform_btn = tkinter.ttk.Button(self.mainframe, text='Waveform Graph', command=print('Temp Waveform'))
-        _waveform_btn.grid(row=3, column=1)
+        _waveform_btn.grid(row=4, column=0)
 
         #RT60 cycling graph button, temporary command that will use modulo counter to alternate low-mid-high
         _cycle_btn = tkinter.ttk.Button(self.mainframe, text='Cycle RT60 Graphs', command=(print('Temp Cycle')))
-        _cycle_btn.grid(row=3, column=2)
+        _cycle_btn.grid(row=5, column=0)
 
         #RT60 combine graph button, temporary command that will overlay each of the RT60 graphs
         #This button goes between resonance label and rt60 cycle button on the provided example, so it is row 5
         _combine_btn = tkinter.ttk.Button(self.mainframe, text='Combine RT60 Graphs', command=print('Temp Combine'))
-        _combine_btn.grid(row=5, column=2)
+        _combine_btn.grid(row=6, column=0)
 
         ##Labels
         #
