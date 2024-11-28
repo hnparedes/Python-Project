56293dc9962029ad576b6a28dc5d1443f67923d8
Hugo <62671886+hnparedes@users.noreply.github.com>
2024-11-28 09:51:15 -0500

    Fixed format on __main__.py

--- src/spidam/__main__.py
+++ src/spidam/__main__.py
@@ -2,16 +2,16 @@ from model import Model
 from view import View
 from controller import Controller
 
-if __name__=="__main__":
-	#Model
+if __name__ == "__main__":
+	# Model
 	model = Model()
 
-	#View
+	# View
 	view = View(model)
 	
-	#Controller
+	# Controller
 	controller = Controller(model, view)
 	view.set_controller(controller)
 
-	#Run root.mainloop
+	# Run root.mainloop
 	view.root.mainloop()
